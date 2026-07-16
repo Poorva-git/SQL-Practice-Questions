790ed6aef6cc9985a87f5ae651ac9ff84bbb89c0
Poorva Soni <poorvasoni2005@gmail.com>
2026-07-16 11:09:45 +0530

    Update schema.sql

--- database/schema.sql
+++ database/schema.sql
@@ -1,6 +1,11 @@
 CREATE TABLE employee (
     emp_id INT PRIMARY KEY,
     emp_name VARCHAR(50),
-    department VARCHAR(50),
+    dept_id INT,
     salary INT
+);
+
+CREATE TABLE department (
+    dept_id INT PRIMARY KEY,
+    dept_name VARCHAR(50)
 );
