7b6c689d14423aca99175b8fa0f8d35182597bdc
Poorva Soni <poorvasoni2005@gmail.com>
2026-07-16 11:23:59 +0530

    Create projects_data.sql

---
+++ database/projects_data.sql
@@ -0,0 +1,11 @@
+-- =========================
+-- Projects Data
+-- =========================
+
+INSERT INTO projects (project_id, project_name, dept_id, budget)
+VALUES
+(201, 'HR Portal', 101, 500000),
+(202, 'E-Commerce Website', 102, 1500000),
+(203, 'Sales Dashboard', 103, 800000),
+(204, 'Finance Management System', 104, 1200000),
+(205, 'Marketing Campaign', 105, 700000);
