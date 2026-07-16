c54d54766171ccd9a5ccdab098cd1a7bf08ff208
Poorva Soni <poorvasoni2005@gmail.com>
2026-07-16 11:22:24 +0530

    Update employee_data.sql

--- database/employee_data.sql
+++ database/employee_data.sql
@@ -1,6 +1,16 @@
-INSERT INTO employee VALUES
-(1, 'Riya', 101, 40000),
-(2, 'Aman', 102, 60000),
-(3, 'Neha', 103, 55000),
-(4, 'Rahul', 102, 70000),
-(5, 'Priya', 101, 45000);
+-- =========================
+-- Employee Data
+-- =========================
+
+INSERT INTO employee (emp_id, emp_name, dept_id, salary, manager_id)
+VALUES
+(1, 'Riya', 101, 40000, NULL),
+(2, 'Aman', 102, 60000, 5),
+(3, 'Neha', 103, 55000, 5),
+(4, 'Rahul', 102, 70000, 2),
+(5, 'Priya', 101, 80000, NULL),
+(6, 'Karan', 104, 50000, 5),
+(7, 'Sneha', 105, 45000, 5),
+(8, 'Arjun', 102, 65000, 2),
+(9, 'Meera', 103, 52000, 3),
+(10, 'Rohit', 101, 48000, 5);
