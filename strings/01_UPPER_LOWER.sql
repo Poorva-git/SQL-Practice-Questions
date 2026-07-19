430eb2dac46f00ee6fa5a2b0b443cd45a8527d1b
Poorva Soni <poorvasoni2005@gmail.com>
2026-07-19 13:13:22 +0530

    Create 01_UPPER_LOWER.sql

---
+++ strings/01_UPPER_LOWER.sql
@@ -0,0 +1,11 @@
+SELECT emp_name, UPPER(emp_name) AS upper_name
+FROM employee;
+
+SELECT emp_name, LOWER(emp_name) AS lower_name
+FROM employee;
+
+SELECT dept_name, UPPER(dept_name)
+FROM department;
+
+SELECT city, LOWER(city)
+FROM employee;
